a936637caa813ad4f65ed39a4f1ba8928e907d2b
Andre0x1 <andrel.silva3357@gmail.com>
2023-08-23 21:52:07 -0300

    Criação das hipoteses

--- DataGetter.py
+++ DataGetter.py
@@ -2,6 +2,8 @@ import requests
 import json
 import time
 import csv
+from datetime import datetime
+
 
 def get_repositories():
     url = "https://api.github.com/graphql"
@@ -86,7 +88,7 @@ def get_info():
 
 
     with open('repos.csv', 'w', newline='', encoding='utf-8') as csv_file:
-        fieldnames = ['nome_repo', 'n_estrelas', 'd_criacao', 'd_atualizacao', 'n_releases', 'linguagem', 'n_issues', 'n_issues_fechadas', 'n_pull_requests']
+        fieldnames = ['nome_repo', 'n_estrelas', 'dias_desde_criacao', 'd_criacao', 'dias_desde_atualizacao', 'd_atualizacao', 'n_releases', 'linguagem', 'n_issues', 'n_issues_fechadas', 'n_pull_requests']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         
 
@@ -94,10 +96,22 @@ def get_info():
 
 
         for repo in data:
+
+            data_criacao =  datetime.strptime(repo['createdAt'], "%Y-%m-%dT%H:%M:%SZ")
+            data_atualizacao =  datetime.strptime(repo['updatedAt'], "%Y-%m-%dT%H:%M:%SZ")
+
+
+            diferenca_criacao = datetime.utcnow() - data_criacao
+            diferenca_atualizacao = datetime.utcnow() - data_atualizacao
+           
+
+
             writer.writerow({
                 'nome_repo': repo['nameWithOwner'],
                 'n_estrelas': repo['stargazers']['totalCount'],
+                'dias_desde_criacao' : diferenca_criacao.days,
                 'd_criacao': repo['createdAt'],
+                'dias_desde_atualizacao' : diferenca_atualizacao.days,
                 'd_atualizacao': repo['updatedAt'],
                 'n_releases': repo['releases']['totalCount'],
                 'linguagem': repo['primaryLanguage']['name'] if repo['primaryLanguage'] else '',
